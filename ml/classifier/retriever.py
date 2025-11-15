6d64daedeacf3ec3033a73ed57be5319291ae32d
Uf/eX <91566452+FlexUnder@users.noreply.github.com>
2025-11-15 18:38:33 +0300

    classifier (old: ml learing) Move

---
+++ ml/classifier/retriever.py
@@ -0,0 +1,20 @@
+from sentence_transformers import SentenceTransformer
+from db.scripts.operations_db import comparing_embeddings
+model = SentenceTransformer(
+    'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+
+def embed(chunks):
+    embeddings = model.encode(chunks)
+
+    return embeddings.tolist()
+
+
+def retrieve_context(user_request):
+    request_embedding = embed(user_request)
+    retrieve = comparing_embeddings(request_embedding)
+    categories = {'юриспруденция': 0, 'маркетинг': 0, 'финансы': 0, 'найм': 0}
+    for e in retrieve:
+        categories[e[0]] += e[1]
+    category = max(categories, key=lambda x: categories[x])
+
+    return category
