614b592dc09fbbbe8213c817ac25a9cdb3a58f81
Alex Smirnov (WSL) <alex.smirnov010407@gmail.com>
2025-11-17 04:06:25 +0300

    Latex + Markdown support in Finance

--- client/src/routes/Marketing.jsx
+++ client/src/routes/Marketing.jsx
@@ -1,5 +1,16 @@
+import MarketingPostCard from "./methods/components/MarketingPostCard"
+
+
 function Marketing() {
-    return <h1>Marketing!</h1>
+    return (
+        <>
+            <h1>Marketing!</h1>
+            <MarketingPostCard 
+                styleType="creative"
+                body={"Для решения задачи определим ежемесячные платежи, при которых сумма долга уменьшается равномерно (линейно)."}
+            />
+        </>
+    )
 }
 
 export default Marketing
